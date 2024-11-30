5a14c3d7a7f0acb258cab8b8835ac5f177fc2ba4
fernandodelperal <fernandodelperal@users.noreply.github.com>
2024-11-30 23:22:32 +0000

    deploy: 0320a9ed9d76bfa74c33ddeb4e287b5187d25430

---
+++ _nuxt/DZgY98np.js
@@ -0,0 +1,36 @@
+import{q as n}from"./B_mOUT37.js";/**
+ * @license lucide-vue-next v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase();/**
+ * @license lucide-vue-next v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var r={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"};/**
+ * @license lucide-vue-next v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=({size:e,strokeWidth:t=2,absoluteStrokeWidth:c,color:o,iconNode:h,name:i,class:k,...l},{slots:s})=>n("svg",{...r,width:e||r.width,height:e||r.height,stroke:o||r.stroke,"stroke-width":c?Number(t)*24/Number(e):t,class:["lucide",`lucide-${w(i??"icon")}`],...l},[...h.map(d=>n(...d)),...s.default?[s.default()]:[]]);/**
+ * @license lucide-vue-next v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=(e,t)=>(c,{slots:o})=>n(y,{...c,iconNode:t,name:e},o);/**
+ * @license lucide-vue-next v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const u=a("ChevronLeftIcon",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);/**
+ * @license lucide-vue-next v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=a("ChevronRightIcon",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);/**
+ * @license lucide-vue-next v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=a("Share2Icon",[["circle",{cx:"18",cy:"5",r:"3",key:"gq8acd"}],["circle",{cx:"6",cy:"12",r:"3",key:"w7nqdw"}],["circle",{cx:"18",cy:"19",r:"3",key:"1xt0gg"}],["line",{x1:"8.59",x2:"15.42",y1:"13.51",y2:"17.49",key:"47mynk"}],["line",{x1:"15.41",x2:"8.59",y1:"6.51",y2:"10.49",key:"1n3mei"}]]);export{u as C,x as S,m as a};
---
+++ _nuxt/DxRl_BtV.js
@@ -0,0 +1,141 @@
+import{u as h}from"./wQpUxeJ4.js";import{e as C,h as i,y as x,j as q,o as l,c as p,a as e,b as d,F as u,d as m,t as s,f as w,G as z,T as A,_}from"./B_mOUT37.js";import{S as M,C as S,a as N}from"./DZgY98np.js";const j={class:"flex flex-col min-h-screen bg-gray-100"},k={class:"bg-white shadow-md p-4 sticky top-0 z-10"},T={class:"flex flex-col sm:flex-row justify-between items-center mb-4"},V={class:"flex justify-between items-center mt-4"},D={class:"text-sm font-semibold"},E={class:"flex-grow p-4"},I={class:"max-w-4xl mx-auto bg-white rounded-lg shadow-md"},B={class:"p-4"},R={class:"text-2xl font-semibold mb-2"},Y={class:"text-gray-600 mb-4"},L={class:"bg-yellow-50 p-4 rounded-lg"},P={class:"whitespace-pre-wrap font-sans text-gray-800"},J={key:0,class:"fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"},O={class:"bg-white p-6 rounded-lg"},Q=C({__name:"241123-colastine",setup(U){const t=[{title:"Ven hermano",tone:"LA",lyrics:`Ven hermano y cántale a Cristo,                           
+a ese Cristo joven que un día nos redimió. 
+Haz de tu amor una plegaria 
+o un simple canto alegre que el Señor escuchará.  
+
+Ven aquí, canta ya 
+no te olvides, tú, de Cristo. 
+Piensa que en la cruz 
+por nosotros Él se dio.  
+
+No te alejes del camino marcado, 
+que Cristo ha señalado para acercarnos a Él. 
+Devuélvele con fe inquebrantable 
+el amor incuestionable que nos ha ofrecido Él.`},{title:"Esto que soy",tone:"SOL",lyrics:`A veces me pregunto por qué yo...
+y sólo me respondes porque quiero
+Es un misterio grande que nos llames
+así tal como somos a tu encuentro.
+Entonces redescubro la verdad:
+mi vida, nuestra vida es un tesoro.
+Se trata entonces sólo de ofrecerte
+con todo nuestro amor esto que somos. 
+
+¿Que te daré? Que te daremos?
+si todo todo es tu regalo.
+Te ofreceré, te ofreceremos esto que somos
+esto que soy, eso te doy. 
+
+Esto que soy eso es lo que te doy,
+esto que somos es lo que te damos.
+Tú no desprecias nuestra vida humilde
+se trata de poner todo en tus manos.
+Aquí van mis trabajos y mi fe,
+mis mates mis bajones y mis sueños,
+y todas las personas que me diste,
+desde mi corazón te las ofrezco. 
+
+Vi tanta gente un domingo de sol,
+me sorprendió el latir de tantas vidas
+Y adiviné tu brazo gigantezco
+y sé que sus historias recibías.
+Por eso el altar luce vino y pan:
+son signo y homenaje de la vida,
+misterio de ofrecerte y recibirnos,
+humanidad que Cristo diviniza.`},{title:"Padre Nuestro Folklórico",tone:"MI",lyrics:`Padre nuestro que estás en el cielo,
+santificado sea tu Nombre;
+venga a nosotros tu Reino;
+hágase tu voluntad 
+en la tierra como en el cielo.
+
+Danos hoy nuestro pan de cada día;
+perdona nuestras ofensas,
+como también nosotros perdonamos 
+a los que nos ofenden;
+no nos dejes caer en la tentación,
+y líbranos del mal.`},{title:"VIDA EN ABUNDANCIA",tone:"MI (capotraste)",lyrics:`Los lirios del campo  y las aves del cielo
+no se preocupan porque están en mis manos
+tené confianza en mí,
+acá estoy junto a vos.
+
+Amá lo que sos y tus circunstancias,
+estoy con vos, con tu cruz en mi espalda,
+todo terminará bien
+yo hago nuevas todas las cosas.
+
+Yo vengo a traerte Vida,
+Vida en abundancia, en abundancia.
+Yo soy el Camino, la Verdad y la Vida,
+Vida en abundancia, en abundancia.
+
+No hice al hombre para que esté solo,
+caminen juntos como hermanos.
+Sopórtense mutuamente,
+ámense unos a otros
+
+La felicidad de la vida eterna
+empieza conmigo en la tierra
+sentite vivo,
+la fiesta del reino comienza acá.
+
+Animo tus sueños y tus proyectos,
+todas tus búsquedas y tus anhelos
+Ya oí tu plegaria,
+arrojate a la esperanza.
+
+Subí a mi barca, navegá mar adentro,
+en lo profundo está lo verdadero.
+Animate a volar,
+la aventura de confiar.`},{title:"CRISTO REINA",tone:"RE",lyrics:`Mi corazón quiere alabar, alabarte
+Mi corazón quiere adorar, adorarte (bis)
+
+Cristo reina, Cristo reina
+Cristo reina, con poder
+
+Mi corazón quiere alabar, alabarte
+Mi corazón quiere adorar, adorarte (bis)
+
+Cristo reina, Cristo reina
+Cristo reina, con amor`},{title:"CANCIÓN DE LA COMUNIÓN",tone:"LA (capotraste)",lyrics:`Ya no eres pan y vino.
+Ahora que eres cuerpo y sangre, vives en mí
+De rodillas yo caigo al contemplar tu bondad
+¿Cómo no te voy a adorar?
+Mientras te pierdes en mis labios
+Tu gracia va inundando todo mi corazón
+Por esa paz que me llena de alegría mi ser
+¿Cómo no te voy a adorar?
+Señor Jesús, mi Salvador
+Amor Eterno, Amor Divino
+Ya no falta nada, lo tengo todo, te tengo a ti
+Ya no falta nada, lo tengo todo, te tengo a ti
+Dueño y Rey del universo
+¿Cómo puede ser posible que busques mi amor?
+Tú tan Grande, yo pequeño y te fijas en mí
+¿Cómo no te voy a adorar?
+De rodillas, yo te pido
+Que el día cuando tú me llames, sea como hoy
+Para mirarte a los ojos y poderte decir
+¿Que, cómo no te voy a adorar?
+Señor Jesús, mi Salvador
+Amor Eterno, Amor Divino
+Ya no falta nada, lo tengo todo, te tengo a ti
+Ya no falta nada, lo tengo todo, te tengo a ti
+Te tengo a ti`},{title:"JUNTO A TI MARIA",tone:"RE",lyrics:`Junto a ti María,
+como un niño quiero estar,
+tómame en tus brazos
+guíame en mi caminar.
+Quiero que me eduques,
+que me enseñes a rezar,
+hazme transparente,
+lléname de paz.
+
+Madre, Madre,
+Madre, Madre. (2x)
+
+Gracias Madre mía
+por llevarnos a Jesús,
+haznos más humildes
+tan sencillos como Tú.
+Gracias Madre mía
+por abrir tu corazón,
+porque nos congregas
+y nos das tu amor.`}],a=i(0),n=i(!1),g=i(""),r=x(()=>t[a.value]);function b(){a.value=(a.value+1)%t.length}function y(){a.value=(a.value-1+t.length)%t.length}function v(){const c=encodeURIComponent("Misa de Comunión y Confirmación, Barrio Bañado: https://fernandodelperal.github.io/medley/songbook/241123-colastine");window.open(`https://wa.me/?text=${c}`,"_blank")}return q(()=>{g.value=window.location.href}),h({title:"Misa de Comunión y Confirmación",meta:[{name:"description",content:"Misa de Comunión y Confirmación, Barrio Bañado"}]}),(c,o)=>(l(),p("div",j,[e("header",k,[e("div",T,[o[3]||(o[3]=e("h1",{class:"text-xl font-bold mb-2 sm:mb-0"}," Misa de Comunión y Confirmación, Barrio Bañado ",-1)),e("button",{class:"bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg flex items-center",onClick:o[0]||(o[0]=f=>n.value=!0)},[d(u(M),{class:"h-5 w-5 mr-2"}),o[2]||(o[2]=m(" Compartir "))])]),e("div",V,[e("button",{class:"flex items-center bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-base font-semibold",onClick:y},[d(u(S),{class:"w-5 h-5 mr-1"}),o[4]||(o[4]=m(" Anterior "))]),e("span",D," Canción "+s(a.value+1)+"/"+s(t.length),1),e("button",{class:"flex items-center bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-base font-semibold",onClick:b},[o[5]||(o[5]=m(" Siguiente ")),d(u(N),{class:"w-5 h-5 ml-1"})])])]),e("main",E,[e("div",I,[e("div",B,[e("h2",R,s(a.value+1)+". "+s(r.value.title),1),e("p",Y," Tono: "+s(r.value.tone),1),e("div",L,[o[6]||(o[6]=e("h3",{class:"text-lg font-semibold mb-2 sr-only"}," Letra: ",-1)),e("pre",P,s(r.value.lyrics),1)])])])]),o[8]||(o[8]=e("footer",{class:"bg-white shadow-md p-4 text-center text-gray-600"},[e("p",{class:"italic"},' "En la Comunión y la Confirmación, nos fortalecemos en Cristo y nos unimos más profundamente a Su Iglesia." ')],-1)),(l(),w(A,{to:"body"},[n.value?(l(),p("div",J,[e("div",O,[o[7]||(o[7]=e("h2",{class:"text-xl font-bold mb-4"}," Compartir ",-1)),e("button",{class:"w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg mb-4",onClick:v}," Compartir por WhatsApp "),e("button",{class:"w-full bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded-lg",onClick:o[1]||(o[1]=f=>n.value=!1)}," Cerrar ")])])):z("",!0)]))]))}}),H=_(Q,[["__scopeId","data-v-3baa1869"]]);export{H as default};
